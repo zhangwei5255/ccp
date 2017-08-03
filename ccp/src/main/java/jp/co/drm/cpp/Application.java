be20d5b68c2078831d6488408b0f2fbd4d86ef85
zhang <zhang@LTJP-SERVER08>
2017-08-03 19:04:53 +0900

    デフォルトのlogback→log4j2

--- ccp/src/main/java/jp/co/drm/cpp/Application.java
+++ ccp/src/main/java/jp/co/drm/cpp/Application.java
@@ -1,7 +1,8 @@
 package jp.co.drm.cpp;
 
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
