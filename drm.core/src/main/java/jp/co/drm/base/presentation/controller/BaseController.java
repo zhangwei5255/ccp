96ce3a6be8283498183fefb628815f2740f6ff6b
zhang <zhang@LTJP-SERVER08>
2017-08-29 18:15:58 +0900

    単項目チェック

--- drm.core/src/main/java/jp/co/drm/base/presentation/controller/BaseController.java
+++ drm.core/src/main/java/jp/co/drm/base/presentation/controller/BaseController.java
@@ -3,7 +3,6 @@ package jp.co.drm.base.presentation.controller;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
 import java.util.Properties;
 import java.util.stream.Stream;
@@ -19,8 +18,6 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.context.support.WebApplicationContextUtils;
 
 import jp.co.drm.util.ConstVals;
 import jp.co.drm.util.MsgUtils;
@@ -33,14 +30,17 @@ public abstract class BaseController {
 	@Autowired
 	HttpServletRequest request;
 
+	@Autowired
+	MessageSource messageSource;
+
 	@ExceptionHandler(MethodArgumentNotValidException.class)
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	private Object[] handleException(HttpServletRequest req, MethodArgumentNotValidException e) {
-		WebApplicationContext ctx = WebApplicationContextUtils.getWebApplicationContext(req.getServletContext());
+		//WebApplicationContext ctx = WebApplicationContextUtils.getWebApplicationContext(req.getServletContext());
 		//Properties itemProperties = (LinkedProperties) ctx.getBean("itemProperties");
 		//ReloadableResourceBundleMessageSource messageSource = (ReloadableResourceBundleMessageSource)ctx.getBean("messageSource");
 		//MessageSource messageSource = (MessageSource)ctx.getBean("messageSource");
-		MessageSource messageSource = ctx.getBean(MessageSource.class);
+		//MessageSource messageSource = ctx.getBean(MessageSource.class);
 
 		BindingResult bindingResult = e.getBindingResult();
 
@@ -60,16 +60,7 @@ public abstract class BaseController {
 				Stream.of(err.getArguments()).forEach(p -> {
 					if (p instanceof DefaultMessageSourceResolvable) {
 						DefaultMessageSourceResolvable dms = (DefaultMessageSourceResolvable) p;
-						/*
-						 * String filed = resouce.get(dms.getCodes()[0]); try {
-						 * filed = new String(filed.getBytes("ISO-8859-1"),
-						 * "UTF-8"); } catch (UnsupportedEncodingException e1) {
-						 * filed = ""; }
-						 */
-
-						//Object filed = applicationProperties.get(dms.getCodes()[0]);
-						Object filed =messageSource.getMessage(dms.getCodes()[0], null, Locale.getDefault());
-						//Object filed2 =messageSource.getMessage(dms.getCodes()[0], null, Locale.JAPAN);
+						Object filed = itemProperties.getProperty(dms.getCodes()[0]);
 						lstArg.add(filed);
 					} else {
 						lstArg.add(p);
@@ -87,23 +78,6 @@ public abstract class BaseController {
 				mapMsg.put(key, msg);
 			}
 		}
-
-/*		List<String> lstMsgOrder = MsgOutputOrder.getInstance().getMapMsgOrder().get(bindingResult.getObjectName());
-
-		if (CollectionUtils.isEmpty(lstMsgOrder)) {
-			return mapMsg.values().toArray(new String[mapMsg.values().size()]);
-		}
-
-		List<String> lstMsg = new ArrayList<String>();
-
-		String msgKey;
-		for (String msgOrder : lstMsgOrder) {
-			msgKey = bindingResult.getObjectName() + ConstVals.STR_SPLIT_DOT + msgOrder;
-			lstMsg.add(mapMsg.get(msgKey));
-		}
-
-		return lstMsg.toArray(new String[mapMsg.values().size()]);
-		*/
 
 		Object[] errMsgs = itemProperties.keySet().stream().filter(k -> mapMsg.containsKey(k))
 								.map(k -> mapMsg.get(k)).toArray();
