9813c9f5acf62b7613dcb62235c79dc15e4e0267
zhang <zhang@LTJP-SERVER08>
2017-09-01 19:23:42 +0900

    認証コードチェック

--- ccp/src/main/webapp/js/login.js
+++ ccp/src/main/webapp/js/login.js
@@ -3,6 +3,8 @@ ccp.model.data.LoginModel = function(dataModel) {
 	self.userId = ko.observable(dataModel.userId);
 	self.pwd = ko.observable(dataModel.pwd);
 	self.isSave = ko.observable(true);		//JSのみ使う
+	self.vervifyCode = ko.observable()		//JSのみ使う
+	self.captchaCss = ko.observable("fa fa-pencil-square-o");		//JSのみ使う
 	//self.messages =  ko.observableArray(dataModel.messages);
 	self.baseDomain = ko.observable(new ccp.model.data.BaseDomain(dataModel));
 }
@@ -18,9 +20,12 @@ ccp.vm.LoginViewModel = function() {
 	self.dataModel = new ccp.model.data.LoginModel(empty);
 	self.handler = new ccp.vm.ErrorViewModel();
 	self.messages = ko.observableArray();
+	self.ctlVervifyCode = new GVerify("v_container");
 
 	self.processing = {
-		onSetPwd : false,
+		onBlurUserId : false,
+		onBlurPwd : false,
+		onValidate : false,
 		doNext : false
 	};
 
@@ -28,16 +33,45 @@ ccp.vm.LoginViewModel = function() {
 		ko.applyBindings(self); // This makes Knockout get to work
 	};
 
-	self.onSetPwd = function(){
-		if (self.processing.onSetPwd) {
+	self.onBlurUserId = function(){
+		if (self.processing.onBlurUserId) {
 			return;
 		}
 
 		var map = myCookie.item(myCookie.keys.login);
 		self.dataModel.pwd(map[self.dataModel.userId()]);
 
-		self.processing.onSetPwd = false;
+		self.processing.onBlurUserId = false;
 	};
+
+	self.onBlurPwd = function(){
+		if (self.processing.onBlurPwd) {
+			return;
+		}
+
+
+		self.processing.onBlurPwd = false;
+	};
+
+
+	self.onValidate = function(){
+		if (self.processing.onValidate) {
+			return;
+		}
+		if(self.dataModel.vervifyCode()){
+			var res = self.ctlVervifyCode.validate(self.dataModel.vervifyCode());
+			if(res){
+				//alert("验证正确");
+				self.dataModel.captchaCss("fa fa-check-circle fa-6 ok");
+			}else{
+				//alert("验证码错误");
+				self.dataModel.captchaCss("fa fa-window-close fa-6 ng");
+			}
+		}
+
+		self.processing.onValidate = false;
+	};
+
 
 	self.doNext = function() {
 		if (self.processing.doNext) {
