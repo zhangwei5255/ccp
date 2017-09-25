d240c1cb80c12399472e51a4927827a090a7f76e
zhang <zhang@LTJP-SERVER08>
2017-09-25 15:46:28 +0900

    ErrorPage追加

---
+++ ccp/src/main/java/jp/co/drm/ccp/web/config/CcpSpringServletContextConfig.java
@@ -0,0 +1,18 @@
+package jp.co.drm.ccp.web.config;
+
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class CcpSpringServletContextConfig {
+
+
+/*	@Bean
+	public GlobalExceptionResolver globalExceptionResolver() {
+		return new GlobalExceptionResolver();
+	}*/
+
+
+
+
+}
